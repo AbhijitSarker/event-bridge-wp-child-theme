98d7d7813d09bfbb5282d6f12ddeb454fa2cbad5
AbhijitSarker <abhijitsarker03@gmail.com>
2024-10-17 13:28:33 +0600

    added css and js files

--- functions.php
+++ functions.php
@@ -5,3 +5,21 @@
 
 //theme title
 add_theme_support( 'title-tag' );
+
+
+//theme css and jquery file calling
+function eb_css_js_file_calling(){
+  wp_enqueue_style( 'eb-style', get_stylesheet_uri());
+
+  wp_register_style( 'bootsrtap', get_template_directory_uri(  ).'/css/bootstrap.css', array(), 'v5.3.3', 'all');
+  wp_register_style( 'custom', get_template_directory_uri(  ).'/css/custom.css', array(), 'v1.0.0', 'all');
+  
+  wp_enqueue_style( 'bootsrtap' );
+  wp_enqueue_style( 'custom' );
+
+  // //jquery
+  wp_enqueue_script( 'jquery', get_template_directory_uri(  ).'/js/bootstrap.js', array(), 'v5.3.3', true );
+  wp_enqueue_script( 'main', get_template_directory_uri(  ).'/js/min.js', array(), 'v1.0.0', true );
+
+}
+add_action( 'wp_enqueue_scripts', 'eb_css_js_file_calling' );
--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 <html lang="<?php language_attributes()?>" class="no-js">
 <head>
   <meta charset="<?php bloginfo( 'charset' )?>">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php wp_head(  )?>
 </head>
